502bfd0554cb75e3d522097d30f2c895be0ba69f
Gyutae Kim <kiimwf0819@gmail.com>
2023-12-01 00:17:24 +0900

    feat(BaseballGame): 도메일 모델 구현

--- src/main/java/baseball/domain/BaseballGame.java
+++ src/main/java/baseball/domain/BaseballGame.java
@@ -1,8 +1,16 @@
 package baseball.domain;
 
+import java.util.List;
+
 public class BaseballGame {
 
-    public BaseballCount requestResult(int number) {
+    public static final int MAX_NUMBER_SIZE = 3;
+    public static final int MIN_NUMBER_RANGE = 1;
+    public static final int MAX_NUMBER_RANGE = 9;
 
+    public BaseballCount startGame(List<Integer> playerNumber) {
+        Computer computer = new Computer();
+        return new BaseballCount(playerNumber, computer.getNumber());
     }
+
 }
