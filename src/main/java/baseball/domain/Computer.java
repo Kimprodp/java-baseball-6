b580c55f19b476ce50cfeed583a98bc75c762ab1
Gyutae Kim <kiimwf0819@gmail.com>
2023-11-30 22:24:29 +0900

    feat(Computer): 도메인 모델 구현

--- src/main/java/baseball/domain/Computer.java
+++ src/main/java/baseball/domain/Computer.java
@@ -1,12 +1,30 @@
 package baseball.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Computer {
 
-    public void createRandomNumber() {
-        
+    private List<Integer> number;
+
+    public Computer() {
+        number = createRandomNumber();
     }
 
-    public int getNumber() {
+    public List<Integer> getNumber() {
+        return number;
+    }
 
+    private List<Integer> createRandomNumber() {
+        List<Integer> randomNumbers = new ArrayList<>();
+        while (randomNumbers.size() < 3) {
+            int number = Randoms.pickNumberInRange(1, 9);
+            if (!randomNumbers.contains(number)) {
+                randomNumbers.add(number);
+            }
+        }
+
+        return randomNumbers;
     }
 }
