e49a2d3751e19a2bbbbd8bee3a2fb09bfccd8353
Gyutae Kim <kiimwf0819@gmail.com>
2023-11-30 23:24:19 +0900

    feat(BaseballCount): 도메인 모델 구현

--- src/main/java/baseball/domain/BaseballCount.java
+++ src/main/java/baseball/domain/BaseballCount.java
@@ -1,12 +1,61 @@
 package baseball.domain;
 
+import java.util.List;
+
 public class BaseballCount {
 
-    public int getStrike() {
+    private static final int DEFAULT_VALUE = 0;
 
+    private int strike;
+    private int ball;
+    private boolean nothing;
+
+    public BaseballCount(List<Integer> playerNumber, List<Integer> computerNumber) {
+        strike = countStrike(playerNumber, computerNumber);
+        ball = countBall(playerNumber, computerNumber);
+        checkNothing();
+    }
+
+    public int getStrike() {
+        return strike;
     }
 
     public int getBall() {
+        return ball;
+    }
 
+    public boolean getNothing() {
+        return nothing;
+    }
+
+    private int countStrike(List<Integer> playerNumber, List<Integer> computerNumber) {
+        int count = DEFAULT_VALUE;
+        for (int i = DEFAULT_VALUE; i < playerNumber.size(); i++) {
+            if (playerNumber.get(i).equals(computerNumber.get(i))) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    private int countBall(List<Integer> playerNumber, List<Integer> computerNumber) {
+        int count = DEFAULT_VALUE;
+        for (int i = DEFAULT_VALUE; i < playerNumber.size(); i++) {
+            if (computerNumber.contains(playerNumber.get(i))) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    private boolean checkNothing() {
+        boolean isNothing = true;
+        if (strike != 0 && ball != 0) {
+            isNothing = false;
+        }
+
+        return isNothing;
     }
 }
