06b952206194830f2bc583c766211b54654b557f
Gyutae Kim <kiimwf0819@gmail.com>
2023-12-04 17:36:14 +0900

    feat(BaseballGame): 공용 인터페이스 추가

--- src/main/java/baseball/domain/BaseballGame.java
+++ src/main/java/baseball/domain/BaseballGame.java
@@ -7,10 +7,25 @@ public class BaseballGame {
     public static final int MAX_NUMBER_SIZE = 3;
     public static final int MIN_NUMBER_RANGE = 1;
     public static final int MAX_NUMBER_RANGE = 9;
+    public static final int WIN_CRITERIA = 3;
 
-    public BaseballCount startGame(List<Integer> playerNumber) {
-        Computer computer = new Computer();
-        return new BaseballCount(playerNumber, computer.getNumber());
+    private Computer computer;
+    private Player player;
+    private BaseballCount baseballCount;
+
+    public void setNewGame() {
+        computer = new Computer();
     }
 
+    public void setPlayer(List<Integer> number) {
+        player = new Player(number);
+    }
+
+    public BaseballCount runGame() {
+        return baseballCount = new BaseballCount(player.getNumber(), computer.getNumber());
+    }
+
+    public boolean checkWin() {
+        return baseballCount.getStrike() == WIN_CRITERIA;
+    }
 }
