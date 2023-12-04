29a73de092b6f852d675cca8e281d7c2fffd8446
Gyutae Kim <kiimwf0819@gmail.com>
2023-12-04 17:32:32 +0900

    refactor(BaseballCount): 메서드 통합

--- src/main/java/baseball/domain/BaseballCount.java
+++ src/main/java/baseball/domain/BaseballCount.java
@@ -8,12 +8,9 @@ public class BaseballCount {
 
     private int strike;
     private int ball;
-    private boolean nothing;
 
     public BaseballCount(List<Integer> playerNumber, List<Integer> computerNumber) {
-        countStrike(playerNumber, computerNumber);
-        countBall(playerNumber, computerNumber);
-        checkNothing();
+        countResult(playerNumber, computerNumber);
     }
 
     public int getStrike() {
@@ -24,29 +21,15 @@ public class BaseballCount {
         return ball;
     }
 
-    public boolean getNothing() {
-        return nothing;
-    }
-
-    private void countStrike(List<Integer> playerNumber, List<Integer> computerNumber) {
+    private void countResult(List<Integer> playerNumber, List<Integer> computerNumber) {
         for (int i = DEFAULT_VALUE; i < playerNumber.size(); i++) {
             if (playerNumber.get(i).equals(computerNumber.get(i))) {
                 strike++;
+                continue;
             }
-        }
-    }
-
-    private void countBall(List<Integer> playerNumber, List<Integer> computerNumber) {
-        for (int i = DEFAULT_VALUE; i < playerNumber.size(); i++) {
-            if (!playerNumber.get(i).equals(computerNumber.get(i)) && computerNumber.contains(playerNumber.get(i))) {
+            if (computerNumber.contains(playerNumber.get(i))) {
                 ball++;
             }
-        }
-    }
-
-    private void checkNothing() {
-        if (strike == 0 && ball == 0) {
-            nothing = true;
         }
     }
 }
